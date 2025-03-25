146b3ffb99ffa409d05d73c7691836c06cd6dc1b
vinay223344 <v647414@gmail.com>
2025-03-25 22:35:28 +0530

    Uploading config.py via API

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    ULCA_API_KEY = "58c1c8d5a0-88f9-4e72-89fc-fc3b0ce428e0"
+    USER_ID = "e98a6b8b96ef4d88811ae4a1a238e983"
+    CALLBACK_URL = "https://dhruva-api.bhashini.gov.in/services/inference/pipeline"
+    AUTHORIZATION_TOKEN = "hiyE1ITwuFQ99KF_MLKh7KSofkeKw-p4Wf-0c8AZzyokIpMe3J3xsrPIFPINxK2a"
